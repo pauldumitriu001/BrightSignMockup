91096a5483259db54d8ef8ed0da88ea837ff3c26
thomas <thomas.carbureanu@gmail.com>
2024-11-28 12:46:42 -0500

    emptied main route

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { DeviceInfoComponent } from './device-info/device-info.component';
+
+export const routes: Routes = [
+  { path: '', component: DeviceInfoComponent }, // Default route
+];
---
+++ frontend/src/app/device-info/device-info.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-device-info',
+  standalone: true, // Mark the component as standalone
+  imports: [CommonModule], // Import CommonModule for Angular directives like *ngIf
+  templateUrl: './device-info.component.html',
+  styleUrls: ['./device-info.component.css'],
+})
+export class DeviceInfoComponent {
+  deviceId: string | null = null;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params: { [x: string]: any }) => {
+      this.deviceId =
+        params['deviceID'] || params['bsnDeviceId'] || 'Unknown Device';
+    });
+  }
+}
